095dbb40169013687ef58f0f4607410fbdaa8612
kanhaiya83 <kanhaiya2778@gmail.com>
2024-11-30 13:16:30 +0000

    fixes

--- src/app/spaces/page.tsx
+++ src/app/spaces/page.tsx
@@ -5,13 +5,13 @@ import Header from "@/components/Header";
 import { publicRequest } from "@/config/request";
 
 const page = async () => {
-  const publicSpacesResponse = await publicRequest.get("/spaces/public")
-  const publicSpaces = publicSpacesResponse.data.data
+  const publicSpacesReq = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + "/spaces/public")
+  const publicSpacesResponse = await publicSpacesReq.json()
+  const publicSpaces = publicSpacesResponse.data
   return <div>
     <Header />
     <SpaceComponent data={publicSpaces}/>
 
   </div>;
 };
-
 export default page;
