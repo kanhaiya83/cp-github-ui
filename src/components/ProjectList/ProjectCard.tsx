9e2080b5fa97688ebb68e2d3dcb17f7fa1eed449
kanhaiya83 <kanhaiya2778@gmail.com>
2024-12-01 08:55:11 +0000

    fixes

--- src/components/ProjectList/ProjectCard.tsx
+++ src/components/ProjectList/ProjectCard.tsx
@@ -16,7 +16,7 @@ const ProjectCard = ({ project, link }: { project: Project; link: string }) => {
         <div className="flex flex-wrap  py-2 px-5 w-full rounded-md  max-md:px-5 max-md:max-w-full  cursor-pointer card-hover-effect">
           <div className="flex flex-auto gap-1">
             <div className="flex flex-col grow gap-2 shrink-0 basis-0 w-fit">
-            <Image className=" rounded-full w-[27%] mx-auto my-4 text-center" width={100} height={100} src={generateAvatar(project.id,"icons")} alt=""/>
+            <Image className=" rounded-full w-[27%] mx-auto my-4 text-center" width={100} height={100} src={generateAvatar(project.id,link.includes("dataset") ? "rings" :"identicon")} alt=""/>
             <h3 className=" text-center">
             {/* <span className="text-sm font-thin text-gray-400">{project.user.username}</span> / */}
                <span className="font-bold text-lg">{project.name}</span> </h3>
