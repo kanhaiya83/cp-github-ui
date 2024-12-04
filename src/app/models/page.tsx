892c86fb230c73fffd78313f6b75fb34456289be
kanhaiya83 <kanhaiya2778@gmail.com>
2024-12-04 03:12:04 +0000

    fixes

--- src/app/models/page.tsx
+++ src/app/models/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ProjectList from "@/components/ProjectList";
 import axios from "axios";
+import { fetchRequest } from "@/config/request";
 
 const Page = async () => {
 
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_GIT_REPO_URL}/groups/datasets/projects/shared`
-  );
-  return <ProjectList rootPath="models" projectList={res.data}/>;
+  const data= await fetchRequest("/models/public")
+  return <ProjectList rootPath="models" projectList={data}/>;
 };
 
 export default Page;
