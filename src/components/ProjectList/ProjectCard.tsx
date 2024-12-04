1ce6292aac8c46c194acd4d34b4525188b4e99da
kanhaiya83 <kanhaiya2778@gmail.com>
2024-12-04 19:33:43 +0000

    likes

--- src/components/ProjectList/ProjectCard.tsx
+++ src/components/ProjectList/ProjectCard.tsx
@@ -5,23 +5,34 @@ import Image from "next/image";
 import { generateAvatar } from "@/utils/avatar";
 import { Model } from "@/types/Model";
 import { Dataset } from "@/types/Dataset";
+import { BsFillHeartFill } from "react-icons/bs";
+import { FiHeart } from "react-icons/fi";
 
-const ProjectCard = ({ project, link }: { project: Model | Dataset; link: string }) => {
+const ProjectCard = ({ project, link, queryData,
+  isLiked,
+  handleLike }: {
+    project: Model | Dataset; link: string, isLiked: boolean
+    queryData?: Model | Dataset;
+    handleLike: (id: string) => Promise<void>
+  }) => {
   return (
-    <Link href={link}>
       <article className="flex flex-col text-sm leading-none rounded-none relative ">
         {/* Top-left Half Border */}
         {/* <div className="absolute top-0 left-0 h-1/2 w-1/2 border-t border-l border-[#62626A] pointer-events-none rounded-l"></div> */}
 
         {/* Bottom-right Half Border */}
         {/* <div className="absolute bottom-0 right-0 h-1/2 w-1/2 border-b border-r border-[#62626A] pointer-events-none rounded-r"></div> */}
+        <button className="flex gap-1 items-center absolute top-4 right-4 z-10 pl-4 py-2" onClick={()=>{handleLike(project._id)}}>
+                { isLiked ?<BsFillHeartFill className="h-5 w-5" /> : <FiHeart className="h-5 w-5" />}
+                <span className="text-gray-400 text-sm"> {queryData?.likes.count || project.likes.count}</span>
+              </button>
         <div className="flex flex-wrap  py-2 px-5 w-full rounded-md  max-md:px-5 max-md:max-w-full  cursor-pointer card-hover-effect">
           <div className="flex flex-auto gap-1">
             <div className="flex flex-col grow gap-2 shrink-0 basis-0 w-fit">
-            <Image className=" rounded-full w-[27%] mx-auto my-4 text-center" width={100} height={100} src={generateAvatar(project._id,link.includes("dataset") ? "rings" :"identicon")} alt=""/>
-            <h3 className=" text-center">
-            {/* <span className="text-sm font-thin text-gray-400">{project.user.username}</span> / */}
-               <span className="font-bold text-lg">{project.name}</span> </h3>
+              <Image className=" rounded-full w-[27%] mx-auto my-4 text-center" width={100} height={100} src={generateAvatar(project._id, link.includes("dataset") ? "rings" : "identicon")} alt="" />
+              <h3 className=" text-center">
+                {/* <span className="text-sm font-thin text-gray-400">{project.user.username}</span> / */}
+                <span className="font-bold text-lg">{project.name}</span> </h3>
               <div className="flex flex-row justify-end items-center gap-2 text-neutral-500">
                 {/* <div className="flex gap-1">
                   <span>
@@ -32,7 +43,7 @@ const ProjectCard = ({ project, link }: { project: Model | Dataset; link: string
                 {/* <GoDotFill /> */}
                 <div className="flex ">
                   <span>
-                     {formatDistanceToNow(project.created_at)} ago
+                    {formatDistanceToNow(project.created_at)} ago
                   </span>
                 </div>
                 {/* <GoDotFill /> */}
@@ -53,8 +64,9 @@ const ProjectCard = ({ project, link }: { project: Model | Dataset; link: string
             </div>
           </div>
         </div>
+        <Link href={link} className="absolute inset-0 w-full h-full">
+        </Link>
       </article>
-    </Link>
   );
 };
 
--- src/components/ProjectList/index.tsx
+++ src/components/ProjectList/index.tsx
@@ -2,28 +2,53 @@
 import FilterProject from "@/components/ProjectList/FilterProject";
 import ProjectCard from "@/components/ProjectList/ProjectCard";
 import SearchProject from "@/components/ProjectList/SearchProject";
+import { authenticatedRequest } from "@/config/request";
+import { useCurrentUser } from "@/hooks/user";
 import { Dataset } from "@/types/Dataset";
 import { Model } from "@/types/Model";
 import { Project } from "@/types/project";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
-const ProjectList = ({ rootPath , projectList }: { rootPath: string , projectList:(Model | Dataset)[] }) => {
+const ProjectList = ({ rootPath, projectList }: { rootPath: string, projectList: (Model | Dataset)[] }) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const { user } = useCurrentUser()
+  const projectsQuery = useQuery({
+    queryKey: ["projectsData"], queryFn: async () => {
+      const projectsResp = await authenticatedRequest.get(`/${rootPath}/public?only_metadata=1`)
+      const data: (Model | Dataset)[] = projectsResp.data
+      return data
+    }, enabled: !!user
+  })
+
+  const handleLike = async (id: string) => {
+    const resp = await toast.promise(authenticatedRequest.get(`/${rootPath}/like/` + id), { pending: "loading..." })
+    if (resp.status == 200) {
+      projectsQuery.refetch()
+    }
+    console.log(resp)
+  }
   return (
     <div>
       <div className="py-10 px-10">
         {rootPath == "datasets" && <FilterProject />}
-        <SearchProject setSearchQuery={setSearchQuery} projects={projectList}/>
+        <SearchProject setSearchQuery={setSearchQuery} projects={projectList} />
         <div className="mb-4 p-4">
           <div className="grid grid-cols-4 gap-x-[3%] gap-y-10">
-            {projectList.filter(d=>d.repository.path_with_namespace.includes(searchQuery)).map((item, index) => (
-              <ProjectCard
+            {projectList.filter(d => d.repository.path_with_namespace.includes(searchQuery)).map((item, index) => {
+              const newData = projectsQuery.data?.find(s=>s._id==item._id)
+              const isLiked =user?._id && newData?.likes.users.includes(user._id )
+              return <ProjectCard
                 key={index}
                 project={item}
                 link={`/${rootPath}/${item.repository.path_with_namespace}`}
-              />
-            ))}
+                queryData={newData}
+                isLiked={!!isLiked}
+                handleLike={handleLike}
+              />}
+            )}
           </div>
         </div>
       </div>
