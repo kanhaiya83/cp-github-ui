98cad6d38cef137189ca54e8b54efe58ab1813a1
kanhaiya83 <kanhaiya2778@gmail.com>
2024-12-05 11:26:07 +0000

    fixes

--- src/app/spaces/[owner]/[repoName]/components/SpaceViewContent.tsx
+++ src/app/spaces/[owner]/[repoName]/components/SpaceViewContent.tsx
@@ -5,9 +5,11 @@ import { Space } from '@/types/Space'
 import LogsContainer from './LogsContainer'
 import { useQuery } from '@tanstack/react-query'
 import { publicRequest } from '@/config/request'
+import { useCurrentUser } from '@/hooks/user'
 
 const SpaceViewContent = ({ data, emptyRepo }: { data: Space, emptyRepo: boolean }) => {
   const [showLogs, setShowLogs] = useState(false)
+  const {user}=  useCurrentUser()
   const statusQuery = useQuery({queryKey:[`status-${data._id}`],queryFn:async()=>{
     const resp = await publicRequest(`/spaces/${data._id}`)
     const respData:Space = resp.data
@@ -16,7 +18,7 @@ const SpaceViewContent = ({ data, emptyRepo }: { data: Space, emptyRepo: boolean
   console.log({statusQuery})
   return (
     <div className="relative w-full mt-4">
-      <button className="py-2 px-3 font-medium text-gray-300 absolute top-0 -translate-y-[125%] right-6 bg-blue-600 rounded-md" onClick={() => { setShowLogs(prev => !prev) }}>{showLogs ? "Hide Logs" : "Show Logs"}</button>
+      {data.user._id == user?._id && <button className="py-2 px-3 font-medium text-gray-300 absolute top-0 -translate-y-[125%] right-6 bg-blue-600 rounded-md" onClick={() => { setShowLogs(prev => !prev) }}>{showLogs ? "Hide Logs" : "Show Logs"}</button>}
       <div className="w-full bg-black border border-gray-800">
       {
         showLogs ? 
