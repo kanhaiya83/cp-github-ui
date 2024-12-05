8f0f995c50dc3136bc7adefd3545b0b40ecf5d8c
kanhaiya83 <kanhaiya2778@gmail.com>
2024-12-05 10:56:52 +0000

    fix

--- src/components/setting/ProfilePopup/index.tsx
+++ src/components/setting/ProfilePopup/index.tsx
@@ -20,7 +20,7 @@ const ProfileSettingsWrapper = ({
     { label: "Create New Access Token", link: "/setting/create-access-token" },
     { label: "Create Deployment", link: "/deployment/new" },
     { label: "Create Dataset", link: "/datasets/new" },
-    { label: "Create Spaces", link: "/spaces/new" },
+    { label: "Create Agent", link: "/spaces/new" },
     { label: "Create Model", link: "/models/new" },
     // { label: "Create Dataset", link: "/datasets/create" },
     { label: "Access Tokens", link: "/setting/access-tokens" },
